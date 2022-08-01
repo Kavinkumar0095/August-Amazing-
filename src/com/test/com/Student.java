682996c3ef98ab0162981080b3ae387320fedfca
preethi2502 <preethijessy305@gmail.com>
2022-08-01 17:39:48 +0530

    data added

---
+++ src/com/test/com/Student.java
@@ -0,0 +1,19 @@
+package com.test.com;
+
+public class Student {
+	public static void main(String[] args) {
+
+		String s = "Welcome";
+		int length = s.length();
+		// string class// returns count of character
+		System.out.println(length);
+
+		//int a[] = new int[0];
+		int a1[] = { 45, 78 };
+		int length2 = a1.length;
+		// array size //return the given size
+		System.out.println(length2);
+
+	}
+
+}
